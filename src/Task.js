234c5958ccba0a1e2b5b219ec4f4670d8f7a380d
Ike Fitria Ramadhani <20104032@ittelkom-pwt.ac.id>
2022-12-18 11:43:01 +0700

    Deployment Assignment

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Ike Fitria Ramadhani" // TODO: replace this
+const studentId = "FE2602016" // TODO: replace this
+
+export { url, studentName, studentId }
